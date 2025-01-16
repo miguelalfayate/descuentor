554000362bd18e98a79fc4f16629b361dad794ef
Miguel Alfayate <miguelalfayateg@gmail.com>
2025-01-16 13:48:48 +0100

    Se crean dtos para la edición de datos de usuarios

---
+++ src/descuentor/Descuentor.Aplicacion/Dtos/UsuarioEditarDto.cs
@@ -0,0 +1,8 @@
+namespace Descuentor.Aplicacion.Dtos;
+
+public class UsuarioEditarDto
+{
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    public string Telefono { get; set; }
+}
