81efedbb19666bfadce5f55f54106535a3a40a2c
Miguel Alfayate <miguelalfayateg@gmail.com>
2025-01-16 13:44:57 +0100

    Se añade el dto ApiErrorResponse

---
+++ src/descuentor/Descuentor.Aplicacion/Dtos/ApiErrorResponse.cs
@@ -0,0 +1,9 @@
+namespace Descuentor.Aplicacion.Dtos;
+
+public class ApiErrorResponse
+{
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public int Status { get; set; }
+    public Dictionary<string, List<string>> Errors { get; set; }
+}
